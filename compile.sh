fa3e6cbec2e49c014076ec19e3dd13fc868cb6dc
Tokusei Noborio <tokusei@nyampass.com>
2016-07-01 15:59:56 +0900

    clear target/macosx directory up

--- compile.sh
+++ compile.sh
@@ -1,4 +1,10 @@
-GOPATH=/Users/tokusei/.go ~/.go/bin/gox -osarch="darwin/amd64" ./...
-# GOPATH=/Users/tokusei/.go ~/.go/bin/gox -osarch="windows/amd64" ./...
+#!/bin/bash
 
-cp robip-tool-go_darwin_amd64 "./Robip Tool.app/Contents/MacOS/robip-tool-go"
+GOPATH=/Users/tokusei/.go ~/.go/bin/gox -osarch="darwin/amd64" ./...
+
+mv robip-tool-go_darwin_amd64 "./target/macosx/Robip Tool.app/Contents/MacOS/robip-tool-go"
+
+rm "./target/macosx/Robip Tool.zip"
+zip -r "./target/macosx/Robip Tool.zip" "./target/macosx/Robip Tool.app"
+
+# GOPATH=/Users/tokusei/.go ~/.go/bin/gox -osarch="windows/amd64" ./...
